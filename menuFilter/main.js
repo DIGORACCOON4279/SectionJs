921bb412fb1c1538fa84bc97ea3f7c2456a7317e
DIGORACCOON4279 <dilema4000@gmail.com>
2023-11-09 19:21:28 -0500

    Adding FilesJSON,HTML, CSS & img

--- menuFilter/main.js
+++ menuFilter/main.js
@@ -9,162 +9,6 @@
 // let priceAggressive = 599;
 // let priceExtreme = 999;
 
-// const outfits = [
-//     {
-//         id: 1,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 2,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 3,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 4,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 5,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 6,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 7,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceAggressive,
-//     },
-//     {
-//         id: 8,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 9,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 10,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 11,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceAggressive,
-//     },
-//     {
-//         id: 12,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 13,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 14,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceAggressive,
-//     },
-//     {
-//         id: 15,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceAggressive,
-//     },
-//     {
-//         id: 16,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 17,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 18,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 19,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceAggressive,
-//     },
-//     {
-//         id: 20,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 21,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 22,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceStreet,
-//     },
-//     {
-//         id: 23,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceAggressive,
-//     },
-//     {
-//         id: 24,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceExtreme,
-//     },
-//     {
-//         id: 25,
-//         title: "Street",
-//         availability: "New Arrivals",
-//         pricing: priceAggressive,
-//     }
-// ]
-
-
-// console.log(outfits.id);
-
 // class product{
 //     constructor(id, title, price, stock, color){
 //         this.id = id;
@@ -188,6 +32,14 @@
 //         }
 //     }
 // }
+
+fetch('outfits.json')
+  .then(response => response.json())
+  .then(data => {
+    // Aquí puedes trabajar con los datos de productos
+    console.log(data);
+  })
+  .catch(error => console.error('Error al cargar el archivo JSON:', error));
 
 
   // Función para aplicar filtros
